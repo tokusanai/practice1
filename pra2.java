330b20d4949c720ee4beee0fcf1b24e023676a0e
tokusanai <k.tokusanai.sys22@morijyobi.ac.jp>
2022-09-15 10:37:41 +0900

    pra2.javaに出力処理追記

---
+++ pra2.java
@@ -0,0 +1,9 @@
+public class pra2{
+    public static void main(String[] args) {
+        int cnt=0;
+        while(cnt<100){
+            System.out.println("Hello World");
+            cnt++;
+        }
+    }
+}
